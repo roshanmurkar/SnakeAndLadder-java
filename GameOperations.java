d2372a052c9cae7c5b70f21a27aa3e9ccb73e1c9
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-09 16:37:01 +0530

    initialization

---
+++ GameOperations.java
@@ -0,0 +1,12 @@
+public class GameOperations {
+	public double startPosition;
+
+	public GameOperations(double startPosition) {
+		super();
+		this.startPosition = startPosition;
+	System.out.println("Player Start position - "+startPosition);
+	}
+	
+	
+	
+}
--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,13 @@
-
+//import java.util.*;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+		double startPosition=0;
+		double endPosition=100;
+		GameOperations startGame = new GameOperations(startPosition);
+		
 	}
 
 }
